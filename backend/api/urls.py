66590b50ee4b8dcdacbe65b0dff8c29bc83c7e5a
Бабаев Роман <apbhueta@yahoo.com>
2023-11-07 22:33:27 +0800

    Foodgram v1.0

---
+++ backend/api/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from api.views import RecipesViewSet, TagsViewSet, UsersViewSet, \
+    IngredientsViewSet
+
+router = routers.DefaultRouter()
+router.register('recipes', RecipesViewSet, basename='recipes')
+router.register('tags', TagsViewSet)
+router.register('users', UsersViewSet)
+router.register('ingredients', IngredientsViewSet)
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('auth/', include('djoser.urls.authtoken')),
+]
